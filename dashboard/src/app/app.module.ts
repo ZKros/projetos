36abe9543fd2fb62711f8354959e4b40fe7c5ecc
Daniel <danielsilva25@live.com>
2022-07-04 16:52:44 -0300

    teste

---
+++ dashboard/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { DashboardModule } from './dashboard';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule, DashboardModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}

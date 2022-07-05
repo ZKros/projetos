280e938f3c5d88b1439acabb2323df7e17ecd300
Daniel <danielsilva25@live.com>
2022-07-05 13:54:02 -0300

    Finalizado

--- projeto-final/src/app/app.module.ts
+++ projeto-final/src/app/app.module.ts
@@ -3,16 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardModule } from './dashboard';
+import { CalculadoraModule } from './calculadora';
+import { ConversorModule } from './conversor';
+import { TarefasModule } from './tarefas';
+import { JogoDaVelhaModule } from './jogo-da-velha';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    DashboardModule,
+    CalculadoraModule,
+    ConversorModule,
+    TarefasModule,
+    JogoDaVelhaModule,
+    AppRoutingModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
